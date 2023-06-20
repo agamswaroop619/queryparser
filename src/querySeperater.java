06685ca605ef3d36f085a6f80e4753f40dd8986f
agamswaroop619 <agam.swaroop619@gmail.com>
2023-06-20 17:11:21 +0530

    New modifications for manual handling

--- src/querySeperater.java
+++ src/querySeperater.java
@@ -7,7 +7,15 @@ import java.util.Scanner;
         System.out.println("Please write the Query");
         String query = sc.nextLine();
         String[] words = query.split(" ");
-
+        int selectIndex=-1, fromIndex=-1;
+        for(int i=0;i<words.length;i++){
+            if(words[i]=="select"){
+                selectIndex=i;
+            }
+            if(words[i]=="from"){
+                fromIndex=i;
+            }
+        }
 
     }
 }
