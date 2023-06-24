3e2b381d7029fa1b188ad186e4154176851d4647
agamswaroop619 <agam.swaroop619@gmail.com>
2023-06-24 17:00:35 +0530

    Manual handling

--- src/querySeperater.java
+++ src/querySeperater.java
@@ -1,21 +1,41 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
  class querySeparator {
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please write the Query");
-        String query = sc.nextLine();
-        String[] words = query.split(" ");
-        int selectIndex=-1, fromIndex=-1;
-        for(int i=0;i<words.length;i++){
-            if(words[i]=="select"){
-                selectIndex=i;
-            }
-            if(words[i]=="from"){
-                fromIndex=i;
-            }
-        }
+     public static void main(String args[]) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter your SQL query:");
 
-    }
-}
+         String sqlQuery = sc.nextLine();
+         sc.close();
+         System.out.println("Given Query is " + sqlQuery);
+         String fullQuery = sqlQuery.trim();
+
+         // Splitting the SQL query into multiple blocks
+         int selectIndex = fullQuery.toUpperCase().indexOf("SELECT");
+         int fromIndex = fullQuery.toUpperCase().indexOf("FROM");
+         int whereIndex = fullQuery.toUpperCase().indexOf("WHERE");
+
+         String selectBlock = fullQuery.substring(selectIndex + "SELECT".length(), fromIndex).trim();
+         String fromBlock = fullQuery.substring(fromIndex + "FROM".length(), whereIndex).trim();
+         String whereBlock = fullQuery.substring(whereIndex + "WHERE".length()).trim();
+
+         // printing the separated blocks
+         System.out.println("Set of column's contain Select Block is : " + selectBlock);
+         System.out.println("Table in From Block is : " + fromBlock);
+         System.out.println("Condition for Where Block is : " + whereBlock);
+         //Now converting into respective array's
+         String[] select = selectBlock.split(", ");
+         System.out.println("Array of Select block is " + Arrays.toString(select));
+         //System.out.println(select[1]);
+
+         //Array for From block
+         String[] from = fromBlock.split(" ");
+         System.out.println("Array of From block is " + Arrays.toString(from));
+
+         //Array for Where Block
+         String[] where = whereBlock.split(" ");
+         System.out.println("Array of Where block is " + Arrays.toString(where));
+     }
+ }
