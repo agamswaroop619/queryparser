61362e26762b6a0abad5af2aade761eaee5ecb9b
agamswaroop619 <agam.swaroop619@gmail.com>
2023-06-24 17:17:11 +0530

    Manual handling v1

--- src/querySeperater.java
+++ src/querySeperater.java
@@ -1,41 +1,66 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
- class querySeparator {
+class QuerySeparator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your SQL query:");
 
-     public static void main(String args[]) {
-         Scanner sc = new Scanner(System.in);
-         System.out.println("Enter your SQL query:");
+        String sqlQuery = sc.nextLine();
+        sc.close();
+        System.out.println("Given Query is " + sqlQuery);
+        String fullQuery = sqlQuery.trim();
+        int selectIndex = fullQuery.toUpperCase().indexOf("SELECT");
+        int fromIndex = fullQuery.toUpperCase().indexOf("FROM");
+        int whereIndex = fullQuery.toUpperCase().indexOf("WHERE");
+        int havingIndex = fullQuery.toUpperCase().indexOf("HAVING");
+        int groupByIndex = fullQuery.toUpperCase().indexOf("GROUP BY");
+        int orderByIndex = fullQuery.toUpperCase().indexOf("ORDER BY");
 
-         String sqlQuery = sc.nextLine();
-         sc.close();
-         System.out.println("Given Query is " + sqlQuery);
-         String fullQuery = sqlQuery.trim();
+        String selectBlock = fullQuery.substring(selectIndex + "SELECT".length(), fromIndex).trim();
+        String fromBlock = fullQuery.substring(fromIndex + "FROM".length(), whereIndex).trim();
+        String whereBlock = fullQuery.substring(whereIndex + "WHERE".length()).trim();
+        String havingBlock = "";
+        String groupByBlock = "";
+        String orderByBlock = "";
 
-         // Splitting the SQL query into multiple blocks
-         int selectIndex = fullQuery.toUpperCase().indexOf("SELECT");
-         int fromIndex = fullQuery.toUpperCase().indexOf("FROM");
-         int whereIndex = fullQuery.toUpperCase().indexOf("WHERE");
+        if (havingIndex != -1) {
+            havingBlock = fullQuery.substring(havingIndex + "HAVING".length(), groupByIndex).trim();
+        }
 
-         String selectBlock = fullQuery.substring(selectIndex + "SELECT".length(), fromIndex).trim();
-         String fromBlock = fullQuery.substring(fromIndex + "FROM".length(), whereIndex).trim();
-         String whereBlock = fullQuery.substring(whereIndex + "WHERE".length()).trim();
+        if (groupByIndex != -1) {
+            groupByBlock = fullQuery.substring(groupByIndex + "GROUP BY".length(), orderByIndex).trim();
+        }
 
-         // printing the separated blocks
-         System.out.println("Set of column's contain Select Block is : " + selectBlock);
-         System.out.println("Table in From Block is : " + fromBlock);
-         System.out.println("Condition for Where Block is : " + whereBlock);
-         //Now converting into respective array's
-         String[] select = selectBlock.split(", ");
-         System.out.println("Array of Select block is " + Arrays.toString(select));
-         //System.out.println(select[1]);
+        if (orderByIndex != -1) {
+            orderByBlock = fullQuery.substring(orderByIndex + "ORDER BY".length()).trim();
+        }
 
-         //Array for From block
-         String[] from = fromBlock.split(" ");
-         System.out.println("Array of From block is " + Arrays.toString(from));
+        // Printing the separated blocks
+        System.out.println("Set of columns in SELECT block: " + selectBlock);
+        System.out.println("Table in FROM block: " + fromBlock);
+        System.out.println("Condition for WHERE block: " + whereBlock);
+        System.out.println("Condition for HAVING block: " + havingBlock);
+        System.out.println("Columns in GROUP BY block: " + groupByBlock);
+        System.out.println("Columns in ORDER BY block: " + orderByBlock);
 
-         //Array for Where Block
-         String[] where = whereBlock.split(" ");
-         System.out.println("Array of Where block is " + Arrays.toString(where));
-     }
- }
+
+        String[] select = selectBlock.split(", ");
+        System.out.println("Array of SELECT block: " + Arrays.toString(select));
+
+        String[] from = fromBlock.split(" ");
+        System.out.println("Array of FROM block: " + Arrays.toString(from));
+
+        String[] where = whereBlock.split(" ");
+        System.out.println("Array of WHERE block: " + Arrays.toString(where));
+
+        String[] having = havingBlock.split(" ");
+        System.out.println("Array of HAVING block: " + Arrays.toString(having));
+
+        String[] groupBy = groupByBlock.split(", ");
+        System.out.println("Array of GROUP BY block: " + Arrays.toString(groupBy));
+
+        String[] orderBy = orderByBlock.split(", ");
+        System.out.println("Array of ORDER BY block: " + Arrays.toString(orderBy));
+    }
+}
